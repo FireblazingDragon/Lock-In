0c0b989b5cf144e4a6ac02eaaa8f33673b5bfebe
olebsbo <yeonjun.anthony.lee@gmail.com>
2026-02-23 13:09:28 -0500

    Fixed version (hopefully)
    Please work

---
+++ pirate-goose-shooter/Actors/enemy_spawner.gd
@@ -0,0 +1,9 @@
+extends Node2D
+@export var enemy : PackedScene
+
+
+	#Creates an enemy continuously with a certain time delay based on the timer node
+func _on_spawn_timer_timeout() -> void:
+	var spawned_enemy = enemy.instantiate()
+	spawned_enemy.global_position = global_position
+	get_parent().add_child(spawned_enemy)
